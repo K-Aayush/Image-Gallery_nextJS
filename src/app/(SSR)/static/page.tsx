01341c7f85e1ce63005669248f23eee902dce4b2
Aayush <ayushkarkee6@gmail.com>
2023-07-06 18:38:27 +0545

    Dynamuc route

--- src/app/(SSR)/static/page.tsx
+++ src/app/(SSR)/static/page.tsx
@@ -1,5 +1,11 @@
 import { UnsplashImg } from "@/models/unsplashImg";
 import Image from "next/image";
+import Link from "next/link";
+import { Alert } from "@/components/bootstrap";
+
+export const metadata = {
+    title: 'Static Fetching - Image Gallery',
+  }
 
 export default async function StaticPage() {
     const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY);
@@ -10,12 +16,17 @@ export default async function StaticPage() {
 
     return(
         <div className="d-flex flex-column align-items-center">
+            <Alert>
+                This page fetches <strong>fetches data statically</strong>.
+                Everytime you refresh this page you will get same image. 
+            </Alert>
             <Image className="rounded shadow mw-100 h-100"
             src={image.urls.raw}
              width={width}
              height={height}
              alt={image.description}
              />
+             by <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
         </div>
     );
 }
---
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -0,0 +1,34 @@
+import { UnsplashImg } from "@/models/unsplashImg";
+import Image from "next/image";
+import styles from "./page.module.css"
+
+// export const revalidate = 0;
+
+interface PageProps {
+    params: {topic: string},
+    // searchParams: { [key: string]: string | string[] | undefined}
+}
+
+
+export default async function TopicPage({params: {topic}}: PageProps) {
+    const response = await fetch(`https://api.unsplash.com/photos/random?query=${topic}&count=30&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
+    const images: UnsplashImg[] = await response.json();
+
+    return(
+        <div>
+            <h1>{topic}</h1>
+            {
+                images.map(image => (
+                    <Image
+                    src={image.urls.raw}
+                    width={250}
+                    height={250}
+                    alt={image.description}
+                    key={image.urls.raw}
+                    className={styles.image}
+                    />
+                ))
+            }
+        </div>
+    )
+}
