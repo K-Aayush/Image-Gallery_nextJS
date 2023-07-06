1c2a9b8512a212ffee3f375127e96013f0838913
Aayush <ayushkarkee6@gmail.com>
2023-07-06 19:08:08 +0545

    fetched data dynamic route

--- src/app/(SSR)/topics/[topic]/page.tsx
+++ src/app/(SSR)/topics/[topic]/page.tsx
@@ -1,31 +1,42 @@
 import { UnsplashImg } from "@/models/unsplashImg";
 import Image from "next/image";
 import styles from "./page.module.css"
+import { Metadata } from "next";
 
 // export const revalidate = 0;
 
 interface PageProps {
-    params: {topic: string},
+    params: { topic: string },
     // searchParams: { [key: string]: string | string[] | undefined}
 }
 
+export function generateMetadata({ params: { topic } }: PageProps): Metadata {
+    return {
+        title: topic + " - Image Gallery"
+    }
+}
 
-export default async function TopicPage({params: {topic}}: PageProps) {
+export function generateStaticParams() {
+    return ["health", "fitness", "coding"].map(topic => ({ topic }))
+}
+
+
+export default async function TopicPage({ params: { topic } }: PageProps) {
     const response = await fetch(`https://api.unsplash.com/photos/random?query=${topic}&count=30&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
     const images: UnsplashImg[] = await response.json();
 
-    return(
+    return (
         <div>
             <h1>{topic}</h1>
             {
                 images.map(image => (
                     <Image
-                    src={image.urls.raw}
-                    width={250}
-                    height={250}
-                    alt={image.description}
-                    key={image.urls.raw}
-                    className={styles.image}
+                        src={image.urls.raw}
+                        width={250}
+                        height={250}
+                        alt={image.description}
+                        key={image.urls.raw}
+                        className={styles.image}
                     />
                 ))
             }
--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
 
 const NavBar = () => {
   const pathname = usePathname();
@@ -22,6 +22,11 @@ const NavBar = () => {
             <Nav.Link as={Link} href='/static' active={pathname === "/static"}>Static</Nav.Link>
             <Nav.Link as={Link} href='/dynamic' active={pathname === "/dynamic"}>Dynamic</Nav.Link>
             <Nav.Link as={Link} href='/isr' active={pathname === "/isr"}>ISR</Nav.Link>
+            <NavDropdown title="topics" id="topics-dropdown">
+              <NavDropdown.Item as={Link} href="/topics/health">Health</NavDropdown.Item>
+              <NavDropdown.Item as={Link} href="/topics/fitness">Fitness</NavDropdown.Item>
+              <NavDropdown.Item as={Link} href="/topics/coding">Coding</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
